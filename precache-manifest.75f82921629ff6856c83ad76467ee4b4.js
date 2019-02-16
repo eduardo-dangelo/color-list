0c932f5fd517e3d6dc575c364ff998788f05e921
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2019-02-16 19:46:12 +0000

    Updates

---
+++ precache-manifest.75f82921629ff6856c83ad76467ee4b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b9611fdbecba38a08c1",
+    "url": "/color-list/static/js/runtime~main.2b9611fd.js"
+  },
+  {
+    "revision": "58e30abb534f1fd5d3e4",
+    "url": "/color-list/static/js/main.58e30abb.chunk.js"
+  },
+  {
+    "revision": "88dce12aafddfda0e096",
+    "url": "/color-list/static/js/2.88dce12a.chunk.js"
+  },
+  {
+    "revision": "58e30abb534f1fd5d3e4",
+    "url": "/color-list/static/css/main.d532e22a.chunk.css"
+  },
+  {
+    "revision": "6a5b33c1d2324f0dcbd9ffa3ed8bd385",
+    "url": "/color-list/index.html"
+  }
+];
